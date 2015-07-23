92e59b242f3b851e847a859bae4f763e894ec0e6
Michał Sajnóg <michal.sajnog@hotmail.com>
2015-07-23 18:15:17 +0200

    base structure

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Docser</title>
+        <link href="//fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet" type="text/css">
+    </head>
+    <body>
+        @yield('body')
+    </body>
+</html>
